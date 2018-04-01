e384e293de0f4644b1ecb53453340e9f92b268a5
tidjungs <waewprach.s@ku.th>
2018-04-01 18:32:59 +0700

    add .env

--- scripts_node/connection.js
+++ scripts_node/connection.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
+
 
 const getConnection = async () => {
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'go-cute',
+    host: process.env.HOSTNAME,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.DB_NAME,
   });
   return connection;
 }
