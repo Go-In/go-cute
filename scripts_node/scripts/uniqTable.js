86546b6d185b7cd16ef8a34b03d206b72517b285
komcal <kom.sujautra@gmail.com>
2018-04-27 00:58:01 +0700

    uniq table

---
+++ scripts_node/scripts/uniqTable.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2/promise');
+const getConnection = require('../connection');
+
+const main = async () => {
+  const tableName = process.argv[2];
+  const fields = process.argv[3];
+
+  console.log(`Uniqe Table ${tableName} by ${fields}.`);
+  const uniqTable = `${tableName}_uniq`;
+  const connection = await getConnection();
+
+  await connection.execute(`CREATE TABLE ${uniqTable} LIKE ${tableName};`);
+  console.log(`create table ${uniqTable}.`);
+  await connection.execute(`ALTER TABLE ${uniqTable} ADD UNIQUE(${fields});`);
+  console.log('Inserting...');
+  await connection.execute(`INSERT IGNORE INTO ${uniqTable} SELECT * FROM ${tableName};`);
+  await connection.end();
+  console.log('Completed.')
+}
+
+main();
+
+ 
