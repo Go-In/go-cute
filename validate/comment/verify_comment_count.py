1e6fa36052c31a373523127b0f3306ab372d82a0
psychoAB <natchanan.th@outlook.com>
2018-05-01 21:55:43 +0700

    added verify_comment_count

---
+++ validate/comment/verify_comment_count.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+import sys, pymysql
+
+# <----------------- prepare
+print('preparing')
+
+post_table_name = 'post'
+comment_table_name = 'comment'
+timestamp = '1522515600'
+
+# <----------------- get param
+print('getting param')
+
+f = open(sys.argv[1], 'r')
+
+user_list = f.readlines()
+
+f.close()
+
+f = open(sys.argv[2], 'r')
+
+env = f.readlines()
+
+f.close()
+
+host = env[0]
+user = env[1]
+pwd = env[2]
+db_name = env[3]
+
+host = host[host.find('=') + 1: -1]
+user = user[pwd.find('=') + 1: -1]
+pwd = pwd[pwd.find('=') + 1: -1]
+db_name = db_name[db_name.find('=') + 1: -1]
+
+init_len = len(user_list)
+
+# <----------------- db
+db = pymysql.connect( host = host , user = user, password = pwd, db = db_name, use_unicode=True, charset="utf8mb4")
+
+c = db.cursor()
+
+while len(user_list) > 0:
+    target_id = user_list[0]
+    user_list.remove(target_id)
+
+    if target_id[0] == '#':
+        continue
+
+    if target_id[0] == '\n':
+        print()
+        continue
+
+    target_id = target_id[:-1]
+
+# <----------------- post
+    post_count_command = "SELECT SUM(`comment_count`) FROM `" + post_table_name + "` WHERE timestamp > " + timestamp + " AND user_id = " + target_id
+
+    c.execute(post_count_command, [])
+
+    post_count = c.fetchone()[0]
+
+# <----------------- comment
+    comment_count_command = "SELECT COUNT(*) FROM `" + comment_table_name + "` WHERE `commented_id` = " + target_id
+
+    c.execute(comment_count_command, [])
+
+    comment_count = c.fetchone()[0]
+
+# <----------------- result
+    if post_count == None or comment_count == None:
+        print(str(init_len - len(user_list)) + ' ' + target_id)
+        
+        continue
+
+    print(str(init_len - len(user_list)) + ' ' + target_id + ' ' + str(bool(float(post_count) * 110.0 / 100.0 >= comment_count) \
+                                                                     & bool(float(post_count) * 90.0 / 100.0 <= comment_count)) \
+                                                                     + ' ' + str(post_count) + ' ' + str(comment_count) )
+
+db.close()
